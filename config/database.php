243b9cb15be61615643b6eb105f7eea9129e4b78
Spb-Webmaster <spb-webmaster@mail.ru>
2025-03-19 16:26:28 +0300

    19-mar-25-3

--- config/database.php
+++ config/database.php
@@ -44,12 +44,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '10.16.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'zavodelia'),
+            'username' => env('DB_USERNAME', 'zavodelia'),
+            'password' => env('DB_PASSWORD', '7p!dJ3BlsnIN'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
